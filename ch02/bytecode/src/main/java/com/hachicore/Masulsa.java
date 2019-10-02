cfe4b3db2794eae059e0d1405510b1dcebdaeb98
8c6t <nkw8620@gmail.com>
2019-10-02 13:39:28 +0900

    2.3 javaagent 실습

--- ch02/bytecode/src/main/java/com/hachicore/Masulsa.java
+++ ch02/bytecode/src/main/java/com/hachicore/Masulsa.java
@@ -3,9 +3,13 @@ package com.hachicore;
 public class Masulsa {
 
     public static void main(String[] args) {
-        // 바이트 코드 변환
+//        ClassLoader classLoader = Masulsa.class.getClassLoader();
+//        TypePool typePool = TypePool.Default.of(classLoader);
+//        // 바이트 코드 변환
 //        try {
-//            new ByteBuddy().redefine(Moja.class)
+//            new ByteBuddy().redefine(
+//                    typePool.describe("com.hachicore.Moja").resolve(),
+//                    ClassFileLocator.ForClassLoader.of(classLoader))
 //                    .method(named("pullOut")).intercept(FixedValue.value("Rabbit"))
 //                    .make().saveIn(new File("D:\\workspace\\java\\the_java\\ch02\\bytecode\\target\\classes\\"));
 //        } catch (IOException e) {
