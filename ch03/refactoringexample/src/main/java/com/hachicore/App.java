2c64f613651f979e7fe8a612802f3839ad2419cc
8c6t <nkw8620@gmail.com>
2019-10-05 15:58:47 +0900

    3.4 리플렉션 API 2부: 클래스 정보 수정, 실행

--- ch03/refactoringexample/src/main/java/com/hachicore/App.java
+++ ch03/refactoringexample/src/main/java/com/hachicore/App.java
@@ -1,22 +1,40 @@
 package com.hachicore;
 
-import java.util.Arrays;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 /**
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) throws ClassNotFoundException {
-        // Arrays.stream(MyBook.class.getAnnotations()).forEach(System.out::println);
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException, NoSuchFieldException {
+        Class<?> bookClass = Class.forName("com.hachicore.Book");
+//        Constructor<?> constructor = bookClass.getConstructor(null);
+//        Book book = (Book) constructor.newInstance();
+        Constructor<?> constructor = bookClass.getConstructor(String.class);
+        Book book = (Book) constructor.newInstance("hachicore");
+        System.out.println(book);
 
-        Arrays.stream(Book.class.getDeclaredFields()).forEach(f -> {
-            Arrays.stream(f.getAnnotations()).forEach(a -> {
-                if (a instanceof MyAnnotation) {
-                    MyAnnotation myAnnotation = (MyAnnotation) a;
-                    System.out.println(myAnnotation.value());
-                    System.out.println(myAnnotation.number());
-                }
-            });
-        });
+        Field a = Book.class.getDeclaredField("A");
+        System.out.println(a.get(null));
+        a.set(null, "AAAAAAAA");
+        System.out.println(a.get(null));
+
+        Field b = Book.class.getDeclaredField("B");
+        b.setAccessible(true);
+        System.out.println(b.get(book));
+        b.set(book, "BBBBBBBB");
+        System.out.println(b.get(book));
+
+        Method c = Book.class.getDeclaredMethod("c");
+        c.setAccessible(true);
+        c.invoke(book);
+
+        Method sum = Book.class.getDeclaredMethod("sum", int.class, int.class);
+        int invoke = (int) sum.invoke(book, 1, 2);
+        System.out.println(invoke);
+
     }
 }
--- ch03/refactoringexample/src/main/java/com/hachicore/Book.java
+++ ch03/refactoringexample/src/main/java/com/hachicore/Book.java
@@ -1,42 +1,23 @@
 package com.hachicore;
 
-// @MyAnnotation(name="hachicore", number=8620)
-@MyAnnotation("hachicore")
 public class Book {
 
-    @MyAnnotation
-    private String a = "a";
+    public static String A = "A";
 
-    private static String b = "BOOK";
-
-    private static final String C = "BOOK";
-
-    public String d = "d";
-
-    protected  String e = "e";
+    public String B = "B";
 
     public Book() {
     }
 
-    // @MyAnnotation
-    @AnotherAnnotation
-    public Book(String a, String d, String e) {
-        this.a = a;
-        this.d = d;
-        this.e = e;
+    public Book(String b) {
+        B = b;
     }
 
-    private void f() {
-        System.out.println("F");
+    private void c() {
+        System.out.println("C");
     }
 
-    @AnotherAnnotation
-    public void g() {
-        System.out.println("g");
+    public int sum(int left , int right) {
+        return left + right;
     }
-
-    public int h() {
-        return 100;
-    }
-
 }
