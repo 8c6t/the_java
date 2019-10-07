760f405e84626a626a5b73a776347c9b5ff551ee
8c6t <nkw8620@gmail.com>
2019-10-07 12:42:56 +0900

    4.3 다이나믹 프록시 실습

--- ch04/demo-spring-di/src/test/java/com/hachicore/demospringdi/BookServiceTest.java
+++ ch04/demo-spring-di/src/test/java/com/hachicore/demospringdi/BookServiceTest.java
@@ -2,15 +2,38 @@ package com.hachicore.demospringdi;
 
 import org.junit.Test;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
 public class BookServiceTest {
 
-    BookService bookService = new BookServicProxy(new DefaultBookService());
+    BookService bookService = (BookService) Proxy.newProxyInstance(
+            BookService.class.getClassLoader(),
+            new Class[]{BookService.class},
+            new InvocationHandler() {
+                BookService bookService = new DefaultBookService();
+                @Override
+                public Object invoke(Object o, Method method, Object[] objects) throws Throwable {
+                    if (method.getName().equals("rent")) {
+                        System.out.println("aaaa");
+                        Object invoke = method.invoke(bookService, objects);
+                        System.out.println("bbbb");
+                        return invoke;
+                    }
+
+                    return method.invoke(bookService, objects);
+                }
+            }
+    );
 
     @Test
     public void di() {
         Book book = new Book();
         book.setTitle("spring");
         bookService.rent(book);
+
+        bookService.returnBook(book);
     }
 
 }
